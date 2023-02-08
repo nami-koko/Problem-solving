6d4781ea69f4aaa4526f7b86bff048e9f2a3ea3e
Nami Ko <86053064+nami-koko@users.noreply.github.com>
2023-02-08 23:27:35 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 51.36 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        Stack<Integer> stack = new Stack<>();
+        for(int i : arr) {
+            if(stack.size() == 0 || stack.peek() != i) {
+                stack.add(i);
+            } 
+        }
+        int[] answer = new int[stack.size()];
+        for(int i = stack.size() - 1; i >= 0; i --) {
+            answer[i] = stack.pop();
+        }
+        return answer;
+    }
+}
